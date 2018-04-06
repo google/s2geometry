9d1425526630f4444504387b3363375e2f71bd8f
Jesse Rosenstock <jmr@google.com>
2018-04-06 15:57:53 -0400

    Add S2Polyline move constructor and assignment.

--- src/s2/s2polyline.h
+++ src/s2/s2polyline.h
@@ -47,6 +47,12 @@ class S2Polyline final : public S2Region {
   // or Decode().
   S2Polyline();
 
+#ifndef SWIG
+  // S2Polyline is movable, but only privately copyable.
+  S2Polyline(S2Polyline&&);
+  S2Polyline& operator=(S2Polyline&&);
+#endif  // SWIG
+
   // Convenience constructors that call Init() with the given vertices.
   explicit S2Polyline(const std::vector<S2Point>& vertices);
   explicit S2Polyline(const std::vector<S2LatLng>& vertices);
