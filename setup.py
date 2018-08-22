59a7d23e00dcedb8cc141acd566b89797d7a050e
Justin Poehnelt <justin.poehnelt@gmail.com>
2018-08-22 12:45:46 -0600

    add git version

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import setup, Distribution
 
 
@@ -6,9 +7,18 @@ class BinaryDistribution(Distribution):
         return True
 
 
+def get_git_version():
+    commit = os.environ.get("TRAVIS_COMMIT")
+
+    if commit is None:
+        return ""
+
+    return ".dev+{}".format(commit[:7])
+
+
 setup(
     name='s2geometry',
-    version="0.0.0",
+    version="0.0.0" + get_git_version(),
     packages=['s2geometry'],
     package_dir={'s2geometry': 'build/python'},
     package_data={'': ['_pywraps2.so']},
