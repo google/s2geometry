d4fb4d390f89ceccbc98822f3a0d0ba01212444a
Jesse Rosenstock <jmr@google.com>
2019-02-11 15:30:02 -0500

    Use reinterpret_cast

--- src/s2/s2loop.cc
+++ src/s2/s2loop.cc
@@ -644,7 +644,8 @@ bool S2Loop::DecodeInternal(Decoder* const decoder,
     defined(_M_IX86)
   bool is_misaligned = false;
 #else
-  bool is_misaligned = ((intptr_t)decoder->ptr() % sizeof(double) != 0);
+  bool is_misaligned =
+      reinterpret_cast<intptr_t>(decoder->ptr()) % sizeof(double) != 0;
 #endif
   if (within_scope && !is_misaligned) {
     vertices_ = const_cast<S2Point *>(reinterpret_cast<const S2Point*>(
