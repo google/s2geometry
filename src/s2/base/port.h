5e3e2a04fd5e561cafa4d940bddd7bc72d50fe03
Jesse Rosenstock <jmr@google.com>
2019-02-26 16:42:11 -0500

    Use size_t in aligned_malloc

--- src/s2/base/port.h
+++ src/s2/base/port.h
@@ -892,21 +892,20 @@ inline void UnalignedCopy64(const void *src, void *dst) {
       !defined(SWIG)) ||                                                \
      ((__GNUC__ >= 3 || defined(__clang__)) && defined(__ANDROID__)) || \
      defined(__ASYLO__))
-inline void *aligned_malloc(size_t size, int minimum_alignment) {
+inline void *aligned_malloc(size_t size, size_t minimum_alignment) {
 #if defined(__ANDROID__) || defined(OS_ANDROID) || defined(__ASYLO__)
   return memalign(minimum_alignment, size);
 #else  // !__ANDROID__ && !OS_ANDROID && !__ASYLO__
-  void *ptr = nullptr;
   // posix_memalign requires that the requested alignment be at least
   // sizeof(void*). In this case, fall back on malloc which should return memory
   // aligned to at least the size of a pointer.
-  const int required_alignment = sizeof(void*);
+  const size_t required_alignment = sizeof(void*);
   if (minimum_alignment < required_alignment)
     return malloc(size);
-  if (posix_memalign(&ptr, static_cast<size_t>(minimum_alignment), size) != 0)
-    return nullptr;
-  else
+  void *ptr = nullptr;
+  if (posix_memalign(&ptr, minimum_alignment, size) == 0)
     return ptr;
+  return nullptr;
 #endif
 }
 
@@ -916,7 +915,7 @@ inline void aligned_free(void *aligned_memory) {
 
 #elif defined(_MSC_VER)  // MSVC
 
-inline void *aligned_malloc(size_t size, int minimum_alignment) {
+inline void *aligned_malloc(size_t size, size_t minimum_alignment) {
   return _aligned_malloc(size, minimum_alignment);
 }
 
