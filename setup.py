9dd13901bc481f1f8253356e49416eca4ca5cf1b
zacharyburnett <zburnett@stsci.edu>
2026-04-21 16:12:04 -0400

    enable py_limited_api via cmake args and setuptools option

--- setup.py
+++ setup.py
@@ -16,18 +16,20 @@ setuptools.setup(
             # (it could be a subfolder)
             source_dir=str(Path(__file__).parent.absolute()),
             cmake_configure_options=[
-                                        # This option points CMake to the right Python interpreter, and helps
-                                        # the logic of FindPython3.cmake to find the active version
-                                        f"-DPython3_ROOT_DIR={Path(sys.prefix)}",
-                                        '-DCALL_FROM_SETUP_PY:BOOL=ON',
-                                        '-DBUILD_SHARED_LIBS:BOOL=OFF',
-                                        '-DCMAKE_POSITION_INDEPENDENT_CODE=ON',
-                                        '-DWITH_PYTHON=ON',
-                                        '-DBUILD_TESTS=OFF',
-                                        '-DFETCH_ABSEIL=ON',
-                                    ]
-        )
+                # This option points CMake to the right Python interpreter, and helps
+                # the logic of FindPython3.cmake to find the active version
+                f"-DPython3_ROOT_DIR={Path(sys.prefix)}",
+                "-DCALL_FROM_SETUP_PY:BOOL=ON",
+                "-DBUILD_SHARED_LIBS:BOOL=OFF",
+                "-DCMAKE_POSITION_INDEPENDENT_CODE=ON",
+                "-DWITH_PYTHON=ON",
+                "-DBUILD_TESTS=OFF",
+                "-DFETCH_ABSEIL=ON",
+                "-DWITH_PYTHON_LIMITED_API=ON",
+            ]
+        ),
     ],
+    options={"bdist_wheel": {"py_limited_api": "cp310"}},
     cmdclass=dict(
         # Enable the CMakeExtension entries defined above
         build_ext=cmake_build_extension.BuildExtension,
