c5214398ae3b71a27ce727d732f4e9f5d89e1ceb
abhinavgautam01 <abgautam1017@gmail.com>
2026-05-11 11:59:40 +0530

    fix: single-expression allocation + clang-format

--- src/s2/s2point_array.h
+++ src/s2/s2point_array.h
@@ -45,9 +45,8 @@ inline UniqueS2PointArray MakeS2PointArrayForOverwrite(int32_t size) {
   // Note that even `std::make_unique_for_overwrite<S2Point[]>(size)`
   // zero-initializes the memory, so we use `::operator new` directly.
   ABSL_DCHECK_GE(size, 0);
-  void* raw = ::operator new(size * sizeof(S2Point),
-                             std::align_val_t(alignof(S2Point)));
-  S2Point* const p = static_cast<S2Point*>(raw);
+  S2Point* p = static_cast<S2Point*>(::operator new(
+      size * sizeof(S2Point), std::align_val_t(alignof(S2Point))));
   return UniqueS2PointArray(p);
 }
 
