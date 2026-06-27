d48934e030d67077e3482830b38f5e50269239be
David Eustis <deustis@aurora.tech>
2026-06-27 18:03:01 +0000

    pybind: Move bind_s2cap before bind_s2latlng/s2cell_id
    S2Cap only depends on s1angle, s1chord_angle, and s2point, all of which
    are already bound at that point. Order bindings by fewest dependencies.

--- src/python/module.cc
+++ src/python/module.cc
@@ -39,14 +39,14 @@ PYBIND11_MODULE(s2geometry_bindings, m) {
   // Deps: s1angle, s2point
   bind_s1chord_angle(m);
 
+  // Deps: s1angle, s1chord_angle, s2point
+  bind_s2cap(m);
+
   // Deps: s1angle, s2point
   bind_s2latlng(m);
 
   // Deps: s1angle, s2point, s2latlng, r2point, r2rect
   bind_s2cell_id(m);
-
-  // Deps: s1angle, s1chord_angle, s2point
-  bind_s2cap(m);
 
   // Deps: r2rect, s1chord_angle, s2cap, s2cell_id, s2latlng, s2point
   bind_s2cell(m);
